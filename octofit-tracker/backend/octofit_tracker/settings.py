9983931f5f647b993814a30fb43f158b1679e92a
agb <62859364+AGBENITEpers@users.noreply.github.com>
2025-04-29 00:10:07 +0000

    modified:   octofit-tracker/backend/octofit_tracker/settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'djongo',
+    'djongo',
     'corsheaders',
     'octofit_tracker',
 ]
